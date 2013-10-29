c047c087237d07330c734b824bfb996ec5996af3
Xiaohe Hu <huxiaohe113@gmail.com>
2013-10-29 14:48:15 -0400

    Initial Commit

---
+++ xhMotionWithPlist/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  xhMotionWithPlist
+//
+//  Created by Xiaohe Hu on 10/29/13.
+//  Copyright (c) 2013 Xiaohe Hu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ xhMotionWithPlist/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  xhMotionWithPlist
+//
+//  Created by Xiaohe Hu on 10/29/13.
+//  Copyright (c) 2013 Xiaohe Hu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
